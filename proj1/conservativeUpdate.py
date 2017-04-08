e1f5d20e8586c6efca06c972f6c19d353102c22e
sainidivya <sainid@stanford.edu>
2017-04-08 09:22:56 -0700

    conservativeUpdate attempted changes

--- proj1/conservativeUpdate.py
+++ proj1/conservativeUpdate.py
@@ -51,7 +51,7 @@ def nonDecreasingOrder(hashTable1, hashTable2, hashTable3, hashTable4, i):
       incrementSlot = byteArray[j + 1]
 
 
-      #increment the count 
+
       if(j == 0):
         countFrequencies.append(hashTable1[incrementSlot])  
       elif(j == 1):
@@ -60,8 +60,9 @@ def nonDecreasingOrder(hashTable1, hashTable2, hashTable3, hashTable4, i):
         countFrequencies.append(hashTable3[incrementSlot])
       elif(j == 3):
         countFrequencies.append(hashTable4[incrementSlot])
-
+    
     minFrequency = min(countFrequencies)
+    print countFrequencies
     if(x == 9050):
       minFreq9050 = minFrequency
     if(isHeavyHitter(minFrequency)):
@@ -85,52 +86,54 @@ def countMinSketch(i):
     #for each of the 4 independent hash tables
     x = numFrequencyPair[0]
     frequency = numFrequencyPair[1]
-    currentTableFrequencies = []
+    
 
-    for j in range(4):
+    for inc in range(int(frequency)):
+      currentTableFrequencies = []
 
-      newX = str(x) + str(i-1)
-      
-      
-      #Calculate the MD5 score of the resulting string
-      
+      for j in range(4):
 
-      hexHash = hashlib.md5(newX).hexdigest()
-      
-      #The hash value is the j-th byte of the score.
-      #incrementSlot is the decimal value 
-      byteArray = bytearray.fromhex(hexHash)
-      incrementSlot = byteArray[j + 1]
-      
-      
-  
-      if(j == 0):
-        currentTableFrequencies.append(hashTable1[incrementSlot])
-      elif(j == 1):
-        currentTableFrequencies.append(hashTable2[incrementSlot])
-      elif(j == 2):
-        currentTableFrequencies.append(hashTable3[incrementSlot])
-      elif(j == 3):
-        currentTableFrequencies.append(hashTable4[incrementSlot])
+        newX = str(x) + str(i-1)
+        
+        
+        #Calculate the MD5 score of the resulting string
+        
 
-
-
-    lowestCurrentCount = min(currentTableFrequencies)
-    updateIndices = []
-    for x in range(4):
-    	if(currentTableFrequencies[x] == lowestCurrentCount):
-    		updateIndices.append(x)
+        hexHash = hashlib.md5(newX).hexdigest()
+        
+        #The hash value is the j-th byte of the score.
+        #incrementSlot is the decimal value 
+        byteArray = bytearray.fromhex(hexHash)
+        incrementSlot = byteArray[j + 1]
+        
+        
+    
+        if(j == 0):
+          currentTableFrequencies.append(hashTable1[incrementSlot])
+        elif(j == 1):
+          currentTableFrequencies.append(hashTable2[incrementSlot])
+        elif(j == 2):
+          currentTableFrequencies.append(hashTable3[incrementSlot])
+        elif(j == 3):
+          currentTableFrequencies.append(hashTable4[incrementSlot])
 
 
-   	for num in updateIndices:
-   		if(num == 0):
-   			hashTable1[incrementSlot] += frequency
-   		elif(num == 1):
-   			hashTable2[incrementSlot] += frequency
-   		elif(num == 2):
-   			hashTable3[incrementSlot] += frequency
-   		elif(num == 3):
-   			hashTable4[incrementSlot] += frequency
+      lowestCurrentCount = min(currentTableFrequencies)
+      updateIndices = []
+      for x in range(4):
+      	if(currentTableFrequencies[x] == lowestCurrentCount):
+      		updateIndices.append(x)
+
+
+     	for num in updateIndices:
+     		if(num == 0):
+     			hashTable1[incrementSlot] += 1
+     		elif(num == 1):
+     			hashTable2[incrementSlot] += 1
+     		elif(num == 2):
+     			hashTable3[incrementSlot] += 1
+     		elif(num == 3):
+     			hashTable4[incrementSlot] += 1
 
   return nonDecreasingOrder(hashTable1, hashTable2, hashTable3, hashTable4, i)
 
