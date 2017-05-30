f0df7778667e50b2681a0ed4d6bc4d9e8bcf8211
sainidivya <sainid@stanford.edu>
2017-05-30 02:02:11 -0700

    3b

--- P8/q3.py
+++ P8/q3.py
@@ -4,32 +4,48 @@ import wave
 import sys
 import scipy.io.wavfile as wavfile
 
+def graphInitial():
+	f = open("mmm.wav", "r")
+	sampleRate, data = wavfile.read(f)
+	if len(data.shape) == 2 and data.shape[1] == 2:
+		data = data[:, 1]	# Remove the second channel if it exists
 
-f = open("mmm.wav", "r")
-sampleRate, data = wavfile.read(f)
-if len(data.shape) == 2 and data.shape[1] == 2:
-	data = data[:, 1]	# Remove the second channel if it exists
+	data = data*1.0/np.max(np.abs(data))
+	with open("output.wav", "w") as f:
+		wavfile.write(f, sampleRate, data)
 
-data = data*1.0/np.max(np.abs(data))
-with open("output.wav", "w") as f:
-	wavfile.write(f, sampleRate, data)
+	times = np.arange(len(data))/float(sampleRate)
 
-times = np.arange(len(data))/float(sampleRate)
+	# Make the plot
+	# You can tweak the figsize (width, height) in inches
+	xmin = 1.5
+	xmax = 1.55
+	ymin = -1.0
+	ymax = 1.0
 
-# Make the plot
-# You can tweak the figsize (width, height) in inches
-xmin = 1.5
-xmax = 1.55
-ymin = -1.0
-ymax = 1.0
+	plt.figure(figsize=(30, 4))
+	plt.plot(times, data, color='b') 
+	plt.xlim(times[0], times[-1])
+	plt.axis([xmin,xmax,ymin,ymax])
+	plt.xlabel('time (s)')
+	plt.ylabel('amplitude')
+	plt.savefig('mmm.png', dpi=100)
+	plt.show()
+	plt.clf()
 
-plt.figure(figsize=(30, 4))
-plt.plot(times, data, color='b') 
-plt.xlim(times[0], times[-1])
-plt.axis([xmin,xmax,ymin,ymax])
-plt.xlabel('time (s)')
-plt.ylabel('amplitude')
-plt.savefig('mmm.png', dpi=100)
-plt.show()
-plt.clf()
+def calculateFourier():
+ 	f = open("mmm.wav", "r")
+	sampleRate, data = wavfile.read(f)
+	if len(data.shape) == 2 and data.shape[1] == 2:
+		data = data[:, 1]	# Remove the second channel if it exists
 
+	fourier = np.absolute(np.fft.fft(data))
+	print fourier.shape
+	tupleIndex = range(167872)
+	print fourier[0:30]
+	plt.plot(tupleIndex, fourier)
+	plt.show()
+
+
+#mmm 167872
+calculateFourier()
